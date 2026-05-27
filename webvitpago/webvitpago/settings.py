2b0b91d4d66ec49f41d170dcff3a4ca35304f39c
Leo Andre <leo283441@gmail.com>
2026-05-27 17:03:28 -0500

    implementacion de docker

--- webvitpago/webvitpago/settings.py
+++ webvitpago/webvitpago/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'webvitpago.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'django_pass',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
